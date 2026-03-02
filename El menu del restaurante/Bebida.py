aced5029b5c8dceb39a239918f1041715ead489e
David <riptted@gmail.com>
2026-03-01 21:52:41 -0800

    Agregue carpeta Taberna

--- El menu del restaurante/Bebida.py
+++
@@ -1,9 +0,0 @@
-from Platillo import Platillo
-
-class Bebida(Platillo):
-    def __init__(self, nombre, precio, temperatura):
-        super().__init__(nombre, precio)
-        self.temperatura = temperatura
-
-    def tipo(self):
-        print(f"Tipo: bebida - {self.temperatura}")
--- El menu del restaurante/Comida.py
+++
@@ -1,9 +0,0 @@
-from Platillo import Platillo
-
-class Comida(Platillo):
-    def __init__(self, nombre, precio, categoria):
-        super().__init__(nombre, precio)
-        self.categoria = categoria
-
-    def tipo(self):
-        print(f"Tipo: comida - {self.categoria}")
