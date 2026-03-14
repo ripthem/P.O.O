0d0d72266c87ee59f06cc7dfedbae58e3f362721
ripthem <riptted@gmail.com>
2026-03-13 20:44:01 -0700

    Delete Taberna directory

--- Taberna/Mago.py
+++
@@ -1,9 +0,0 @@
-from Aventurero import Aventurero
-
-class Mago(Aventurero):
-    def __init__(self, nombre, nivel, hechizo):
-        super().__init__(nombre, nivel)
-        self.hechizo = hechizo
-
-    def usar_habilidad(self):
-        print(f"{self.nombre} lanza {self.hechizo}! 🔮")
